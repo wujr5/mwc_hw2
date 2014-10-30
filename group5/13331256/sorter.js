9a07533a7666a8a5e309f603ab8d5d4a828da26b
Valen <wwl0816@qq.com>
2014-10-30 10:37:21 +0800

    v 0.2 终于支持各种坑爹table了好开心
    改善比较算法，可对数字排序；
    支持各种不合规范，如无thead，无th等的table；
    添加了注释。

--- group5/13331256/sorter.js
+++ group5/13331256/sorter.js
@@ -2,7 +2,7 @@
 * @Author: ValenW
 * @Date:   2014-10-28 14:21:40
 * @Last Modified by:   ValenW
-* @Last Modified time: 2014-10-29 16:56:52
+* @Last Modified time: 2014-10-30 10:36:19
 */
 window.onload = function() {
     var tables = getAllTables();
@@ -19,72 +19,102 @@ function makeAllTablesSortable(tables) {
     for (var i = 0; i < tables.length; i++) {
         shortedth[i] = new Array();
         var oThead = tables[i].getElementsByTagName('thead');
-        for (var j = 0; j < oThead.length; j++) {
-            shortedth[i][j] = new Array();
-            var oTh = oThead[j].getElementsByTagName('th');
-            if (oTh.length == 0) oTh = oThead[j].getElementsByTagName('td'); // 为西西里君专门做的兼容优化
-            for (var k = 0; k < oTh.length; k++) {
-                shortedth[i][j][k] = 0;
-                oTh[k].addEventListener('click', toShort);
+        if (oThead.length != 0) { // 有thead
+            for (var j = 0; j < oThead.length; j++) {
+                shortedth[i][j] = 0;
+                var oTh = oThead[j].getElementsByTagName('th');
+                if (oTh.length == 0) oTh = oThead[j].getElementsByTagName('td'); // 为西西里君专门做的兼容优化
+                for (var k = 0; k < oTh.length; k++)
+                    oTh[k].addEventListener('click', toShort);
+            }
+        } else {
+            var oTbody = tables[i].getElementsByTagName('tbody');
+            for (var jj = 0; jj < oTbody.length; jj++) {
+                shortedth[i][jj] = 0;
+                var oTh2 = oTbody[jj].getElementsByTagName('th');
+                if (oTh2.length == 0)
+                    oTh2 = oTbody[jj].getElementsByTagName('tr')[0].getElementsByTagName('td');
+                for (var kk = 0; kk < oTh2.length; kk++)
+                    oTh2[kk].addEventListener('click', toShort);
             }
         }
     }
 }
-
 function toShort(event) {
     // 找到调用的th的i, j, k编号
     var tables = getAllTables();
     var mTable = this.parentNode.parentNode.parentNode;
     var mThead = this.parentNode.parentNode;
     var mTh = this;
+    var noth = false, nothead = false;
+    // 确定table编号i
     for (var i = 0; i < tables.length &&
         tables[i].innerHTML != mTable.innerHTML; i++);
+    // 确定thead/tbody编号j
     var oThead = tables[i].getElementsByTagName('thead');
+    if (oThead.length == 0) {
+        oThead = tables[i].getElementsByTagName('tbody');
+        nothead = true;
+    }
     for (var j = 0; j < oThead.length && 
         oThead[j].innerHTML != mThead.innerHTML; j++);
     var oTbody = tables[i].tBodies[j];
+    // 确定th/td编号k
     var oTh = oThead[j].getElementsByTagName('th');
-    if (oTh.length == 0) oTh = oThead[j].getElementsByTagName('td');
+    if (oTh.length == 0) {
+        oTh = oThead[j].getElementsByTagName('tr')[0].getElementsByTagName('td');
+        noth = true;
+    }
     for (var k = 0; k < oTh.length &&
         oTh[k].innerHTML != mTh.innerHTML; k++);
 
     // 排序并更新tbody
     var oTr = oTbody.rows;
     var retr = new Array();
-    for(var ii = 0; ii < oTr.length; ii++) {
-        retr[ii] = oTr[ii];
-    }
-    if (shortedth[i][j][k]) {
+    for(var ii = 0; ii+nothead < oTr.length; ii++)
+        retr[ii] = oTr[ii+nothead];
+    if (shortedth[i][j] == k+1 || shortedth[i][j] == -k-1) {
         retr.reverse();
     } else {
         retr.sort(getcmp(k)); // 调用sort函数并传入比较函数进行排序
     }
     var reFrag = document.createDocumentFragment();
-    for(var ii = 0; ii < retr.length; ii++) {
+    if (nothead) reFrag.appendChild(oTr[0]);
+    for(var ii = 0; ii < retr.length; ii++)
         reFrag.appendChild(retr[ii]);
-    }
     oTbody.appendChild(reFrag);
 
-    // 更新th和tr
-    this.style.backgroundColor = "#7FFFD4";
-    if (shortedth[i][j][k] == 1) { // 点击的列表已是升序时不同处理
-        this.style.backgroundImage = 'url("images/descend.png")';
-        shortedth[i][j][k] = -1;
-    } else {
-        this.style.backgroundImage = 'url("images/ascend.png")';
-        shortedth[i][j][k] = 1;
+    // 更新tr背景色和th
+    var ooTr = oTbody.getElementsByTagName('tr'); // tr背景色变化
+    var firsttr = nothead ? 1 : 0;
+    for (var jj = firsttr; jj < ooTr.length; jj++) {
+        if ((jj+firsttr) % 2) ooTr[jj].style.backgroundColor = "#FFE4E1";
+        else ooTr[jj].style.backgroundColor = "white";
     }
-    for (var ii = 0; ii < oTh.length; ii++) //对本列中其他th进行还原
-        if (ii != k) {
-            shortedth[i][j][ii] = 0;
+    var oldshortedth = shortedth[i][j] == 0 ? -1 : Math.abs(shortedth[i][j])-1;
+    if (oldshortedth == -1) { // 对本列中th进行初始化
+        for (var ii = 0; ii < oTh.length; ii++) {
             oTh[ii].style.backgroundColor = "#000080";
             oTh[ii].style.backgroundImage = "";
         }
-    var ooTr = oTbody.getElementsByTagName('tr'); // tr背景色变化
-    for (var i = 0; i < ooTr.length; i++) {
-        if (i % 2) ooTr[i].style.backgroundColor = "#FFE4E1";
-        else ooTr[i].style.backgroundColor = "white";
+    } else { // 对本列中之前排序的th初始化
+        oTh[oldshortedth].style.backgroundColor = "#000080";
+        oTh[oldshortedth].style.backgroundImage = "";
     }
+    if (shortedth[i][j] == k+1) { // 点击的列表已是升序时不同处理
+        this.style.backgroundImage = 'url("images/descend.png")';
+        this.style.backgroundRepeat = 'no-repeat';
+        this.style.backgroundPosition = 'right';
+        this.style.cursor = 'pointer';
+        shortedth[i][j] = -k-1;
+    } else {
+        this.style.backgroundImage = 'url("images/ascend.png")';
+        this.style.backgroundRepeat = 'no-repeat';
+        this.style.backgroundPosition = 'right';
+        this.style.cursor = 'pointer';
+        shortedth[i][j] = k+1;
+    }
+    this.style.backgroundColor = "#7FFFD4";
 }
 function getcmp(k) { // 比较函数
     return function cmp(tr1, tr2) {
@@ -104,5 +134,5 @@ function getcmp(k) { // 比较函数
         }
     }
 }
-    // var tables = getAllTables();
-    // makeAllTablesSortable(tables);
+// var tables = getAllTables();
+// makeAllTablesSortable(tables);
